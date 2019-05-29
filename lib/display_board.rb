267a8d18a094c44e59a1b55a231544abda79da47
Andnrew Cheon <cheonandrew113@gmail.com>
2019-05-29 17:55:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,34 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  display_board(board)
+
+  board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+  puts display_board(board)
+
+  board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+  puts display_board(board)
+  
+  board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+  puts display_board(board)
+  
+  board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+  puts display_board(board)
+  
+  board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+  puts display_board(board)
+  
+  board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+  puts display_board(board)
+  
+  board = ["O", " ", " ", " ", "O", " ", " ", " ", "O"]
+  puts display_board(board)
+  
+  board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+  puts display_board(board)
+  
+  board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+  puts display_board(board)
+end
